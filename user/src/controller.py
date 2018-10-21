df605d67579247e5d99085786d1afac93fddd8a9
Willian Frantz <willianluigii@gmail.com>
2018-10-21 17:41:20 -0300

    :sparkles: Created the create_user service

--- user/src/controller.py
+++ user/src/controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from user.src.model import User, UserSerializer
 
 
@@ -8,3 +8,15 @@ users = Blueprint('users', __name__)
 def list_users():
     users = User.query.all()
     return jsonify(UserSerializer(users).data)
+
+
+@users.route('/', methods=['POST'])
+def create_user():
+    name = request.json.get('name', None)
+
+    if not name:
+        return 'name parameter missing', 403
+
+    user = User(name=name).save()
+
+    return jsonify(UserSerializer(user).data), 201
